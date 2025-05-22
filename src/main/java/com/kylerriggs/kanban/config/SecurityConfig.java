6b6a0718730fcb9a03985abcc454090985e159b7
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2025-05-22 19:38:25 -0400

    Move SecurityConfig to config package

--- src/main/java/com/kylerriggs/kanban/config/SecurityConfig.java
+++ src/main/java/com/kylerriggs/kanban/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.kylerriggs.kanban.security;
+package com.kylerriggs.kanban.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
