435c13c8cca9d5f5fa2a1f398cbb08614fd5c28a
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2025-05-25 08:31:46 -0400

    Add findAllByProjectId

--- src/main/java/com/kylerriggs/kanban/issue/IssueRepository.java
+++ src/main/java/com/kylerriggs/kanban/issue/IssueRepository.java
@@ -3,5 +3,8 @@ package com.kylerriggs.kanban.issue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    List<Issue> findAllByProjectId(Long projectId);
 }
