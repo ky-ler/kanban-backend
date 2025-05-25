c18db3b3f36251801e8dde2c26403171d3c8a540
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2025-05-25 08:29:52 -0400

    Include issue summaries in project dto

--- src/main/java/com/kylerriggs/kanban/project/dto/ProjectDto.java
+++ src/main/java/com/kylerriggs/kanban/project/dto/ProjectDto.java
@@ -1,9 +1,13 @@
 package com.kylerriggs.kanban.project.dto;
 
+import com.kylerriggs.kanban.issue.dto.IssueSummaryDto;
+import com.kylerriggs.kanban.user.dto.UserSummaryDto;
+
 public record ProjectDto(Long id,
                          String name,
                          String description,
                          UserSummaryDto createdBy,
                          CollaboratorDto[] collaborators,
+                         IssueSummaryDto[] issues,
                          String dateCreated,
                          String dateModified) { }
