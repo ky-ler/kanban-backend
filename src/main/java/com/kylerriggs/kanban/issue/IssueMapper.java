dc7d9e8d04fe0410bf31e8d60125c30c79f978c4
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2025-10-16 02:17:03 -0400

    Add IssueSummaryDto mapping and refactor ProjectMapper to use UserMapper and IssueMapper

--- src/main/java/com/kylerriggs/kanban/issue/IssueMapper.java
+++ src/main/java/com/kylerriggs/kanban/issue/IssueMapper.java
@@ -2,6 +2,7 @@ package com.kylerriggs.kanban.issue;
 
 import com.kylerriggs.kanban.issue.dto.CreateIssueRequest;
 import com.kylerriggs.kanban.issue.dto.IssueDto;
+import com.kylerriggs.kanban.issue.dto.IssueSummaryDto;
 import com.kylerriggs.kanban.priority.Priority;
 import com.kylerriggs.kanban.project.Project;
 import com.kylerriggs.kanban.status.Status;
@@ -36,6 +37,18 @@ public class IssueMapper {
                 issue.getProject().getId(),
                 issue.getDateCreated()!= null ? issue.getDateModified().toString() : null,
                 issue.getDateModified() != null ? issue.getDateModified().toString() : null
+        );
+    }
+
+    public IssueSummaryDto toIssueSummaryDto(Issue issue) {
+        String assignedToUsername = issue.getAssignedTo() != null ? issue.getAssignedTo().getUsername() : null;
+
+        return new IssueSummaryDto(
+                issue.getId(),
+                issue.getTitle(),
+                issue.getStatus(),
+                issue.getPriority(),
+                assignedToUsername
         );
     }
 
